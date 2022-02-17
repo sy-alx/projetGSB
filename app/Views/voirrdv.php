067f4f1cc66527ebcc55a98d8e7b4b4d6a608625
maxime_chostak <mchostak@yahoo.fr>
2022-02-17 10:10:38 +0100

    indentation et suppression code commenté

--- app/Views/voirrdv.php
+++ app/Views/voirrdv.php
@@ -1,81 +1,46 @@
 <section>
 
     <h1>Voir mes rendez-vous</h1>
-    
-<?php
-    // if (isset($_GET['semaine'])){
-    //     $etat = $_GET['semaine'];
-    // }
 
-    // if (isset($_GET['jour'])){
-    //     $jour = $_GET['jour'];    
-    // }else{
-    //     $jour = date('w');
-    // }
-
-    // if (!isset($etat)){
-    //     $etat = 0;
-    // }
-
-    // if ($etat == 1){
-    //     $jour = $jour + 7;
-
-    // }elseif ($etat == 2){
-    //     $jour = $jour - 7;
-
-    // }elseif ($etat == 0){
-    //     $jour = date('w');
-    // }
-        var_dump($jour);
-
-  //  if(isset($etat)):
-?>
-        <table style="width: 100%;" border="1">
+    <table style="width: 100%;" border="1">
         <thead>
-        <tr>
-        <th><?php          
-            // $sub = '';
-            // for($i=1;$i<=7;$i++){
-            //     $temp = date('d-m-Y', strtotime('+'.($i-$jour).' days'));
-            //     $sub .= $temp."</th><th>"; 
-            // }
-            // echo (substr($sub,0,-4));
-            foreach($createSemaine as $row){
-                echo $row."</th><th>";
-            }
-        ?>
-        </th>
+            <tr>
+                <th>
+                    <?php          
+                        foreach($createSemaine as $row){
+                            echo $row."</th><th>";
+                        }
+                    ?>
+                </th>
+            </tr>
         </thead>
         <tbody>
-        <?php 
-            // $weekStart =  date('Y-m-d', strtotime('+'.(1-$jour).' days')); refaire weekstart et weekend pour la requête SQL
-            // $weekEnd = date('Y-m-d', strtotime('+'.(7-$jour).' days'));
-            $heureCalendrier = 8;
+            <?php 
+                $heureCalendrier = 8;
 
-            for ($i=1; $i<=10;$i++){
-                echo '<tr>';
-                
-                for($j=1;$j<=7;$j++){
+                for ($i=1; $i<=10;$i++){
+                    echo '<tr>';
+                    
+                    for($j=1;$j<=7;$j++){
 
-                       $textForCell = "";
-                        foreach($liste_rdv as $row){
-                            if($row['date_rdv'] == date('Y-m-d', strtotime('+'.($j-$jour).' days')) && $row['heure_rdv'] == $heureCalendrier){
-                                $textForCell .= "Le".$row['date_rdv']." à ".$row['heure_rdv']."h";
+                            $textForCell = "";
+                            foreach($liste_rdv as $row){
+                                if($row['date_rdv'] == date('Y-m-d', strtotime('+'.($j-$jour).' days')) && $row['heure_rdv'] == $heureCalendrier){
+                                    $textForCell .= "Le".$row['date_rdv']." à ".$row['heure_rdv']."h";
+                                }
                             }
-                        }
-                        if(empty($textForCell)) {
-                            $textForCell = "&nbsp";
-                        }
-                        echo '<td> ' . $textForCell . '</td>';
+                            if(empty($textForCell)) {
+                                $textForCell = "&nbsp";
+                            }
+                            echo '<td> ' . $textForCell . '</td>';
+                    }
+                    echo '</tr>';
+                    $heureCalendrier++;
                 }
-                echo '</tr>';
-                $heureCalendrier++;
-            }
-        ?>
+            ?>
         </tbody>
-        </table>
+    </table>
     <?php   
-    //endif
     ?>   
     <br>
     <a href="Voirrdv?semaine=1&jour=<?php echo $jour ?>">Semaine précédente</a>

cbcdf2d6a63b6d8c61a3e38ad4155aff1f4d0f12
maxime_chostak <mchostak@yahoo.fr>
2022-05-09 23:49:18 +0200

    url app et env apiOvh

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/';
+    public $baseURL = 'https://projetgsb.maximechostak.fr/';
 
     /**
      * --------------------------------------------------------------------------
